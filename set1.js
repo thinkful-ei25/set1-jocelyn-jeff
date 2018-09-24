ec5728d27c3dea9983b52afc1757769dbe735ec9
jsantiag <jsantiag@wellesley.edu>
2018-09-24 14:35:11 -0500

    throwing,catching,trying

--- set1.js
+++ set1.js
@@ -1,8 +1,23 @@
 
-
+function getYearOfBirth(age){
+    return(2018-age);
+}
 function createGreeting(name, age){
-    return (`Hi, my name is ${name} and I'm ${age} years old`);
+    const yob = getYearOfBirth(age);
+    if (age < 0){
+        throw new Error("Age can not be negative");
+    
+    }
+    return (`Hi, my name is ${name} and I'm ${age} years old.
+I was born in ${yob}.`);
+    try {
+        const greeting1 = createGreeting('Jo', 4);
+    } catch (error) {
+        console.error(error)
+
+    }
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
+
+
+console.log(createGreeting('Jo', -4));
