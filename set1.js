c989e4096b689950bb8ad8ea6bae010e8109a0f5
jsantiag <jsantiag@wellesley.edu>
2018-09-24 13:44:57 -0500

    greeting1 return

--- set1.js
+++ set1.js
@@ -1,7 +1,8 @@
 
 
 function createGreeting(){
-    console.log('Hi, my name is Jocelyn and I\'m 24 years old');
+    return ('Hi, my name is Jocelyn and I\'m 24 years old');
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
